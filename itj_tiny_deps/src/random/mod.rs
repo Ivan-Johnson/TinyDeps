c325980c93e782b999fde980b5303efd5e32110f
Ivan Johnson <git@IvanJohnson.net>
2025-03-31 20:15:53 -0400

    Add a TODO for using oorandom

--- itj_tiny_deps/src/random/mod.rs
+++ itj_tiny_deps/src/random/mod.rs
@@ -3,3 +3,8 @@ mod random_trait;
 
 pub use from_file::RandomFromFile;
 pub use random_trait::Random;
+
+// TODO: I just found the [oorandom](https://lib.rs/crates/oorandom) crate. It's
+// genuinely tiny, has no dependencies, and seems to do exactly what I want. Add
+// a new implementation of my trait that's a wrapper around it? Gate behind a
+// feature flag, obviously. Possibly even move to an `itj_tiny_deps_3p` crate?
