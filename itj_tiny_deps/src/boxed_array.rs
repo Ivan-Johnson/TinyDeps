ea3c3d5874a0265ee0fa9b53137b45fac4c8b6bf
Ivan Johnson <git@IvanJohnson.net>
2025-06-20 23:56:06 -0400

    Fix failing doc test

--- itj_tiny_deps/src/boxed_array.rs
+++ itj_tiny_deps/src/boxed_array.rs
@@ -2,21 +2,34 @@
 ///
 /// e.g. consider this code:
 ///
-/// ```
-/// let foo = Box::new(["foo", 1_000_000_000]);
+/// ```should_panic
+/// let foo = Box::new(["foo"; 10_000_000]);
 /// ```
 ///
 /// This creates a large array *on the stack*, and *then* copies to the heap. As
-/// such, the above code will likely result in a stack overflow.
+/// such, the above code is expected to result in a stack overflow.
 ///
-/// Using this helper function instead will ensure that the data is created in
-/// place on the heap, thereby preventing the stack overflow.
-pub fn boxed_array<T: Copy, const N: usize>(val: T) -> Box<[T; N]> {
-	let boxed_slice: Box<[T]> = vec![val; N].into_boxed_slice();
+/// Using this helper function, as below, avoids the overflow by ensuring that
+/// the data is created in place on the heap.
+///
+/// ```
+/// use itj_tiny_deps::boxed_array;
+/// let foo: Box<[_; 10_000_000]> = boxed_array![123; 10_000_000];
+/// ```
+#[macro_export]
+macro_rules! boxed_array {
+	($val:expr ; $len:expr) => {{
+		fn boxed_array2<T: Copy, const N: usize>(val: Vec<T>) -> Box<[T; N]> {
+			assert_eq!(val.len(), N);
+			let boxed_slice: ::std::boxed::Box<[T]> = val.into_boxed_slice();
 
-	let ptr: *mut [T; N] = Box::into_raw(boxed_slice) as *mut [T; N];
+			let ptr: *mut [T; N] = ::std::boxed::Box::into_raw(boxed_slice) as *mut [T; N];
 
-	let out: Box<[T; N]> = unsafe { Box::from_raw(ptr) };
+			let out: ::std::boxed::Box<[T; N]> = unsafe { ::std::boxed::Box::from_raw(ptr) };
 
-	out
+			out
+		}
+
+		boxed_array2(vec![$val; $len])
+	}};
 }
